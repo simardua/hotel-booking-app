c44b12001ada66cd8a45d79fd859c97a1196905b
Your Name <you@example.com>
2023-04-17 21:02:25 +0530

    hotel booking system

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import navbar from './navbar.css'
+
+function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">Book It Up!</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/register">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login">Log in</a>
+                        </li>
+                        
+                        
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/screens/Homescreen.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
+
+const Homesceen = () => {
+ 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () =>{
+ 
+      try {
+        const {data: response} = await axios.get('/api/rooms/getallrooms');
+        setData(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+  
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+    <h1> Home Screen </h1>
+      <h1>they are {data.length} rooms</h1>
+
+    </div>
+  )
+}
+
+export default Homesceen;
